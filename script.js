2d492321a4642c5b3d6762ff1d90737b4c279cc9
Brian Uceta <brian.uceta.19@wheels-student.org>
2017-01-17 14:57:12 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    //Write all your jQuery code here
-        $("button").click(function() {
+        $("button").click(function(){
       $("#like").html("You like this project!");
     });
     
